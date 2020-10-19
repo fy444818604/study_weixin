382ee08a3a38ced4ca641ab00e97ea0ec9c25ad1
ahaow <ahao72@qq.com>
2020-10-19 15:44:28 +0800

    wh

--- pages/myexam/result/result.js
+++ pages/myexam/result/result.js
@@ -1,6 +1,6 @@
 // pages/myexam/result/result.js
 const api = require('../../../utils/api')
-
+const WxParse = require('../../../lib/wxParse/wxParse');
 Page({
 
   /**
@@ -8,62 +8,120 @@ Page({
    */
   data: {
     tabIndex: 1,
-    tabArr: [
-      {
+    tabArr: [{
         index: 1,
-        text:"考试结果"
+        text: "考试结果"
       },
       {
         index: 2,
-        text:"答题结果"
+        text: "答题结果"
       },
       {
         index: 3,
-        text:"试题分析"
+        text: "试题分析"
       }
     ],
-    radioItems: [
-      {value: '1', name: '查看全部解析'},
-      {value: '2', name: '只看错题解析', checked: 'true'},
+    radioItems: [{
+        value: '1',
+        name: '查看全部解析',
+        checked: 'true'
+      },
+      {
+        value: '2',
+        name: '只看错题解析',
+      }
     ],
-    endTime: "", 
+    replyTime: "",
+    endTime: "",
     testTime: "",
     userScore: 0,
     svgRate: 0,
     objectiveScore: 0,
     accuracy: 0,
+    middleList: [],
+    paperReportList: [],
   },
 
   handleChangeTab(e) {
     let index = e.currentTarget.dataset.index;
     this.setData({
-      tabIndex: index
+      tabIndex: index,
+      middleList: this.data.middleListLS
     })
+  },
+
+  radioChange(e) {
+    let val = e.detail.value;
+    if (val == "1") {
+      this.setData({
+        middleList: this.data.middleListLS
+      })
+    } else {
+      let ls = JSON.parse(JSON.stringify(this.data.middleListLS))
+      let data = ls.map(item => {
+        item.qstMiddleList.forEach((val, index) => {
+          if (val.status == 0) {
+            item.qstMiddleList.splice(index, 1)
+          }
+        })
+        return item;
+      })
+      this.setData({
+        middleList: data,
+      })
+    }
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    let user= wx.getStorageSync("user")
+    let user = wx.getStorageSync("user")
     let data = {
       id: 1245953,
       userId: user.userId
     }
     api.reportDetail({
+      data: {},
       success: res => {
         console.log("res", res)
         let data = res.data
+        let arr = data.paperMiddleList.filter(item => {
+          return item.type != 4 && item.type != 7
+        })
+        arr = arr.map(item => {
+          if (item.type == 5 || item.type == 2 || item.type == 1) {
+            item.qstMiddleList.forEach(v => {
+              let result1 = v.userAnswer.split("")
+              let result2 = v.optAnswer.split("")
+              v.optionList.forEach(k => {
+                let result3 = result1.includes(k.optOrder)
+                let result4 = result2.includes(k.optOrder)
+                k.isFlag = result3;
+                k.isAll = result4
+                return k;
+              })
+              return v
+            })
+          }
+          return item
+        })
         this.setData({
+          replyTime: data.examPaper.replyTime,
           endTime: data.paperRecord.addTime,
           testTime: 0,
           userScore: data.paperRecord.userScore,
-          svgRate: data.examPaper.score*data.examPaper.passRate*0.01,
+          svgRate: data.examPaper.score * data.examPaper.passRate * 0.01,
           objectiveScore: data.paperRecord.objectiveScore,
-          accuracy: data.paperRecord.accuracy * 100
+          accuracy: data.paperRecord.accuracy * 100,
+          middleListResult: arr,
+          middleList: arr,
+          middleListLS: arr,
+          paperReportList: data.queryPaperReport
         })
       }
-    },data)
+    }, data)
   },
 
   /**
